1c75a98c8ef42fb8dcae894803fac7592c5c2266
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-03-18 21:23:40 -0600

    Add files via upload

---
+++ Ejercicos en Python/Suma_dos_numeros.py
@@ -0,0 +1,13 @@
+n1 = float(input("Ingresa el primer numero: "))
+n2 = float(input("Ingresa el segundo numero: "))
+s = n1 + n2
+print ("El resultado es: ",s)
+
+print()
+
+nombre = (input("Ingresa tu nombre completo: "))
+print("Tu nombre es:", nombre)
+
+ap = (input("Ingresa tu apellido paterno: "))
+am = (input("Ingresa tu apellido materno: "))
+print (nombre + " " + ap + " " + am)
