b7ceac8f8cc00a675bea5428bc1b8fb5e8706215
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-03-21 23:17:35 -0600

    Add files via upload

---
+++ Ejercicos en Python/Conactenar.py
@@ -0,0 +1,6 @@
+x1 = (input("Ingresa un numero:"))
+x2 = (input("Ingresa un numero:"))
+x3 = (input("Ingresa un numero:"))
+
+concatenar = str(x1) + str(x2) + str(x3)
+print("El resultado concatenado es: ", concatenar)
