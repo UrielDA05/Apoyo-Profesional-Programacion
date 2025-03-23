308ed8628d17f762e0744bf5a1670410c11f53e7
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-03-22 22:09:28 -0600

    Add files via upload

---
+++ Ejercicos en Python/ExponenteFuncion.py
@@ -0,0 +1,12 @@
+def exponente():
+    n1 = int(input("Ingresa el numero que desea calcular: "))
+    a = 1
+    b = 2
+    acum = 0
+    while a <= n1:
+        acum += a ** b
+        a += 1
+        b += 1
+    res = acum / n1
+    print("El resultado es: ", res) 
+exponente()       
