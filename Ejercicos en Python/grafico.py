d317b95ec01d8200a475c507beb28b26845cb356
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-05-05 22:14:17 -0600

    Add files via upload

---
+++ Ejercicos en Python/grafico.py
@@ -0,0 +1,6 @@
+from tkinter import*
+raiz =Tk() # la variable raiz toma el valor de modo grafico (ventana)
+raiz.title ('TECNOLOGICO DE ESTUDIOS SUPERIORES DE JILOTEPEC')
+# raiz.resizable (True,True) # se puede ampliar con mouse tanto horizontal y vertical
+raiz.geometry ("700x400") # La ventana esta defina en horizontal y vertical
+raiz.mainloop ()
